ae128de0de322e655e76db060f6a19dc5e6ce60e
j0jds <mrlucassilva7342@gmail.com>
2024-10-08 23:33:11 -0300

    Create component (test)

---
+++ Vue/src/components/MyComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>{{ mensagem }}</h1>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, ref } from 'vue';
+  import axios from 'axios';
+  
+  export default defineComponent({
+    setup() {
+      const mensagem = ref('');
+  
+      axios.get('http://127.0.0.1:8000')
+        .then(response => {
+          mensagem.value = response.data.message;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+  
+      return { mensagem };
+    },
+  });
+  </script>
+  
